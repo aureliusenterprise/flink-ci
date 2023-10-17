9e29998070d5df3ce2b66691c1278865ec839790
wombach <andreas.wombacher@gmail.com>
2023-10-17 19:12:51 +0000

    experiment with 2 devcontainers

---
+++ .taskmanager/postcreate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install dependencies
+poetry install
+
+# Download JAR files if not already present
+while read -r url; do
+    [ -z "$url" ] && continue
+    filename=$(basename "$url")
+    if [ -e jars/"$filename" ]; then
+        echo "File jars/$filename already exists, skipping download."
+        continue
+    fi
+    wget -P jars/ "$url"
+done < jars/manifest
+
+# Add current directory as safe repository location
+git config --global --add safe.directory $PWD
+
+# Install pre-commit hooks
+poetry run pre-commit install
+
+# Install local package for development
+.venv/bin/pip install --no-deps -e .
