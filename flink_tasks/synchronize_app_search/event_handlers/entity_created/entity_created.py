3a24468442a6a10d315f8bc1196e71a16c83f7d2
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2023-12-01 12:51:34 +0100

    Create entity breadcrumbs - When creating an entity, also create breadcrumbs. - Add test scenario

--- flink_tasks/synchronize_app_search/event_handlers/entity_created/entity_created.py
+++ flink_tasks/synchronize_app_search/event_handlers/entity_created/entity_created.py
@@ -1,5 +1,8 @@
+from collections.abc import Generator
 from typing import cast
 
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
 from m4i_atlas_core import Entity
 
 from flink_tasks import AppSearchDocument, EntityMessage
@@ -20,7 +23,76 @@ class EntityDataNotProvidedError(Exception):
         super().__init__(f"Entity data not provided for entity {guid}")
 
 
-def default_create_handler(entity_details: Entity) -> AppSearchDocument:
+def get_documents(
+    query: dict,
+    elastic: Elasticsearch,
+    index_name: str,
+) -> Generator[AppSearchDocument, None, None]:
+    """
+    Yield AppSearchDocument objects from Elasticsearch based on the given query.
+
+    Parameters
+    ----------
+    query : dict
+        The Elasticsearch query used to fetch documents.
+    elastic : Elasticsearch
+        The Elasticsearch client instance.
+    index_name : str
+        The name of the index in Elasticsearch to query.
+
+    Yields
+    ------
+    Generator[AppSearchDocument, None, None]
+        Yields AppSearchDocument instances as they are retrieved from Elasticsearch.
+    """
+    for result in scan(elastic, index=index_name, query=query):
+        yield AppSearchDocument.from_dict(result["_source"])
+
+
+def get_breadcrumbs_of_entity(
+    input_entity: Entity,
+    elastic: Elasticsearch,
+    index_name: str,
+) -> tuple[list[str], list[str], list[str]]:
+    """
+    Update entity breadcrumbs based on the provided input_entity.
+
+    Parameters
+    ----------
+    input_entity : Entity
+        The Entity instance for which breadcrumbs need to be updated.
+    elastic : Elasticsearch
+        The Elasticsearch client for database interaction.
+    index_name : str
+        The name of the index in Elasticsearch to query.
+
+    Returns
+    -------
+    tuple[list[str], list[str], list[str]]
+        A tuple containing lists of updated breadcrumb details - name, GUID, and type.
+    """
+    if input_entity is None:
+        return [], [], []
+
+    # Get parents of entity
+    parent_guids = [x.guid for x in input_entity.get_parents()]
+    breadcrumb_name, breadcrumb_guid, breadcrumb_type = [], [], []
+    # Look up breadcrumbs of parents
+    query = {"query": {"terms": {"breadcrumb_guid": parent_guids}}}
+
+    for document in get_documents(query, elastic, index_name):
+        breadcrumb_name += [*document.breadcrumbname, document.name]
+        breadcrumb_guid += [*document.breadcrumbguid, document.guid]
+        breadcrumb_type += [*document.breadcrumbtype, document.typename]
+
+    return breadcrumb_name, breadcrumb_guid, breadcrumb_type
+
+
+def default_create_handler(
+    entity_details: Entity,
+    elastic: Elasticsearch,
+    index_name: str,
+) -> AppSearchDocument:
     """
     Create an AppSearchDocument instance using the provided entity details.
 
@@ -28,6 +100,10 @@ def default_create_handler(entity_details: Entity) -> AppSearchDocument:
     ----------
     entity_details : Entity
         The entity details to extract the necessary attributes from.
+    elastic : Elasticsearch
+        The Elasticsearch client for database interaction.
+    index_name : str
+        The name of the index in Elasticsearch to query.
 
     Returns
     -------
@@ -39,16 +115,26 @@ def default_create_handler(entity_details: Entity) -> AppSearchDocument:
     qualified_name = attributes["qualifiedName"]
     name = attributes.get("name", qualified_name)
 
+    # Get breadcrumbs
+    b_names, b_guids, b_types = get_breadcrumbs_of_entity(entity_details, elastic, index_name)
+
     return AppSearchDocument(
         id=entity_details.guid,
         guid=entity_details.guid,
         typename=entity_details.type_name,
         name=name,
         referenceablequalifiedname=qualified_name,
+        breadcrumbname=b_names,
+        breadcrumbguid=b_guids,
+        breadcrumbtype=b_types,
     )
 
 
-def create_person_handler(entity_details: Entity) -> AppSearchDocument:
+def create_person_handler(
+    entity_details: Entity,
+    elastic: Elasticsearch,
+    index_name: str,
+) -> AppSearchDocument:
     """
     Create an AppSearchDocument instance for a person entity using the provided entity details.
 
@@ -56,13 +142,17 @@ def create_person_handler(entity_details: Entity) -> AppSearchDocument:
     ----------
     entity_details : Entity
         The person entity details to extract the necessary attributes from.
+    elastic : Elasticsearch
+        The Elasticsearch client for database interaction.
+    index_name : str
+        The name of the index in Elasticsearch to query.
 
     Returns
     -------
     AppSearchDocument
         The created AppSearchDocument instance.
     """
-    result = default_create_handler(entity_details)
+    result = default_create_handler(entity_details, elastic, index_name)
 
     attributes: dict[str, str] = cast(dict, entity_details.attributes.unmapped_attributes)
 
@@ -77,6 +167,8 @@ ENTITY_CREATED_HANDLERS = {"m4i_person": create_person_handler}
 
 def handle_entity_created(
     message: EntityMessage,
+    elastic: Elasticsearch,
+    index_name: str,
 ) -> list[AppSearchDocument]:
     """
     Process the entity creation message and create an AppSearchDocument instance accordingly.
@@ -85,6 +177,10 @@ def handle_entity_created(
     ----------
     message : EntityMessage
         The EntityMessage instance containing the entity creation details.
+    elastic : Elasticsearch
+        The Elasticsearch client for database interaction.
+    index_name : str
+        The name of the index in Elasticsearch to query.
 
     Returns
     -------
@@ -104,4 +200,4 @@ def handle_entity_created(
 
     create_handler = ENTITY_CREATED_HANDLERS.get(entity_details.type_name, default_create_handler)
 
-    return [create_handler(entity_details)]
+    return [create_handler(entity_details, elastic, index_name)]
