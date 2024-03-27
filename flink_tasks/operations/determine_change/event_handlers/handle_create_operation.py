036b4b3b60e1937f9f50cb2d922311b537237b65
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2024-03-27 11:54:45 +0100

    Fix deleting unmappedAttributes in create entity

--- flink_tasks/operations/determine_change/event_handlers/handle_create_operation.py
+++ flink_tasks/operations/determine_change/event_handlers/handle_create_operation.py
@@ -40,8 +40,8 @@ def handle_create_operation(
         raise ValueError(msg)
 
     attributes_dict = entity.attributes.to_dict()
+    del attributes_dict['unmappedAttributes']
     inserted_attributes = list(attributes_dict.keys())
-    del inserted_attributes['unmappedAttributes']
 
     entity_message = EntityMessage.from_change_message(
         change_message=change_message,
