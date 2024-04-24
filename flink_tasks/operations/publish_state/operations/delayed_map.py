211675a40a10ec91d054dfc01c5d34028d4f0608
lorandvatamany <lorand.vatamany@aureliusenterprise.com>
2024-04-24 15:24:52 +0200

    Remove delay on Exception message type

--- flink_tasks/operations/publish_state/operations/delayed_map.py
+++ flink_tasks/operations/publish_state/operations/delayed_map.py
@@ -10,15 +10,18 @@ class DelayedMap(MapFunction):
         super(DelayedMap, self).__init__()
         self.last_timestamp = None
         self.time_to_sleep_s = 1
-        self.time_between_messages_ms = 1000
+        self.time_between_messages_ms = 500
 
     def map(self, value):
+        if isinstance(value, Exception):
+            return value
+
         current_timestamp = time.time()
 
         if self.last_timestamp is not None:
             time_diff_ms = (current_timestamp - self.last_timestamp) * 1000
             if time_diff_ms < self.time_between_messages_ms:
-                logging.warning("Delaying by %s seconds.", self.time_to_sleep_s)
+                logging.debug("Delaying by %s seconds.", self.time_to_sleep_s)
                 time.sleep(self.time_to_sleep_s)
 
         self.last_timestamp = time.time()
